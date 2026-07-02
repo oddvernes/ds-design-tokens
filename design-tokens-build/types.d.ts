73d6d7aa7157289a418c8b4fde1cf1d19ea89ca4
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2026-07-02 12:16:35 +0200

    build theme

---
+++ design-tokens-build/types.d.ts
@@ -0,0 +1,18 @@
+/* build: v1.18.0 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    primary: never;
+    accent: never;
+    neutral: never;
+    mandarin: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
